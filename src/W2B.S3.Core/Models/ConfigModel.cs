c90503186a64c3d7f98d994791e935165193926b
Alex <68811141+letnull19A@users.noreply.github.com>
2025-08-04 00:31:15 +0400

    Feat m config file (#6)
    * feat: запуск webapi в приложении + сообщение приметствия
    
    * refactor: ConfigFilesModule -> ConfigModule
    
    * feat: added loader YAML files
    
    ---------
    
    Co-authored-by: xarlein <alexvolkovdd@gmail.com>

--- src/W2B.S3.Core/Models/ConfigModel.cs
+++ src/W2B.S3.Core/Models/ConfigModel.cs
@@ -1,20 +1,69 @@
-﻿namespace W2B.S3.Core.Models;
+﻿using YamlDotNet.Serialization;
+
+namespace W2B.S3.Core.Models;
 
 public sealed class ConfigModel
 {
-    public Network Network { get; }
-    public RootUser Root { get; }
+    [YamlMember(Alias = "cluster")]
+    public ClusterConfig Cluster { get; set; }
 }
 
-public sealed class Network(ushort port, string ipAddress)
+public sealed class ClusterConfig
 {
-    public ushort Port { get; } = port;
-    public string IpAddress { get; } = ipAddress;
+    [YamlMember(Alias = "name")]
+    public string Name { get; set; }
+
+    [YamlMember(Alias = "mode")]
+    public string Mode { get; set; }
+
+    [YamlMember(Alias = "volume")]
+    public string Volume { get; set; }
+
+    [YamlMember(Alias = "root")]
+    public RootUser Root { get; set; }
+
+    [YamlMember(Alias = "network")]
+    public NetworkConfig Network { get; set; }
+
+    [YamlMember(Alias = "postgres")]
+    public PostgresConfig Postgres { get; set; }
 }
 
-public sealed class RootUser(string name, string password, string token)
+public sealed class RootUser
 {
-    public string Name { get; } = name;
-    public string Password { get; } = password;
-    public string Token { get; } = token;
+    [YamlMember(Alias = "user")]
+    public string User { get; set; }
+
+    [YamlMember(Alias = "password")]
+    public string Password { get; set; }
+
+    [YamlMember(Alias = "token")]
+    public string Token { get; set; }
+}
+
+public sealed class NetworkConfig
+{
+    [YamlMember(Alias = "host")]
+    public string Host { get; set; }
+
+    [YamlMember(Alias = "port")]
+    public ushort Port { get; set; }
+}
+
+public sealed class PostgresConfig
+{
+    [YamlMember(Alias = "user")]
+    public string User { get; set; }
+
+    [YamlMember(Alias = "password")]
+    public string Password { get; set; }
+
+    [YamlMember(Alias = "dataBase")]
+    public string DataBase { get; set; }
+
+    [YamlMember(Alias = "host")]
+    public string Host { get; set; }
+
+    [YamlMember(Alias = "port")]
+    public ushort Port { get; set; }
 }
---
+++ src/W2B.S3.Core/Modules/ConfigModule.cs
@@ -0,0 +1,84 @@
+﻿using System.Text;
+using W2B.S3.Core.Configs;
+using W2B.S3.Core.Interfaces;
+using W2B.S3.Core.Models;
+
+namespace W2B.S3.Core.Modules;
+
+public sealed class ConfigModule(IReadOnlyDictionary<string, string> args) : IControlModule
+{
+    private string _fileName = string.Empty;
+    private string _fileNameExtension = string.Empty;
+    private ConfigModel? _configModel;
+
+    public void Init()
+    {
+    }
+
+    public (string, string, ConfigModel?) Get()
+    {
+        return (_fileName, _fileNameExtension, _configModel);
+    }
+
+    public void Start()
+    {
+        var isConfigParamExist = IsConfigFileDefined();
+
+        if (isConfigParamExist)
+            ConfigsIsDefined();
+        else
+            ConfigsIsNotDefined();
+    }
+
+    public void TakeControl(IControlModule module)
+    {
+        module.Init();
+        module.Start();
+    }
+
+    public void End()
+    {
+        throw new NotImplementedException();
+    }
+
+    private bool IsConfigFileDefined()
+    {
+        return args.ContainsKey("--configFile");
+    }
+
+    private void ConfigsIsNotDefined()
+    {
+        Console.WriteLine("Configuration file is not defined, please set arg --configFile");
+    }
+
+    private void ConfigsIsDefined()
+    {
+        try
+        {
+            args.TryGetValue("--configFile", out _fileName);
+
+            _fileNameExtension = _fileName.Split('.')[^1];
+
+            var cwd = Directory.GetCurrentDirectory();
+
+            if (!File.Exists(_fileName))
+                throw new FileNotFoundException($"""
+                                                 failed to load configs:
+                                                 file name: {_fileName}
+                                                 file ext: {_fileNameExtension}
+                                                 CWD: {cwd}
+                                                 """);
+
+            var fullPath = Path.GetFullPath(_fileName);
+                
+            if (_fileNameExtension == "yaml")
+            {
+                _configModel = YAMLConfigLoader.LoadConfig(fullPath);
+            }
+        }
+        catch (Exception exception)
+        {
+            Console.WriteLine(exception.Message);
+        }
+    }
+}
