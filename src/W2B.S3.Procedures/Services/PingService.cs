b26376f23962d798ddb45fac973ca6110d8d980f
letnull19a <alexvolokovdd@googlemail.com>
2025-09-04 15:26:06 +0400

    feat: добавил процедуру ping

---
+++ src/W2B.S3.Procedures/Services/PingService.cs
@@ -0,0 +1,15 @@
+﻿using W2B.S3.Procedures.Protos;
+
+namespace W2B.S3.Procedures.Services
+{
+    public class PingService : Pinger.PingerBase
+    {
+        public override Task<PingReply> Ping(PingRequest request, Grpc.Core.ServerCallContext context)
+        {
+            return Task.FromResult(new PingReply
+            {
+                Message = "Pong"
+            });
+        }
+    }
+}
